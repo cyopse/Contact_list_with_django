8e4607b3eeb64b5c1aa47ac275b8d362b2b89401
cyopse <lucas.ta@outlook.com>
2024-04-03 16:34:07 -0300

    1 - Pillow foi adicionado aos requerimentos do projeto 2 - A tabela Contact recebeu novos campos e agora um contato pode ter uma foto 3 - A tabela Category foi criada sendo uma foreignKey em Contact

--- djangoapp/contact/admin.py
+++ djangoapp/contact/admin.py
@@ -6,11 +6,17 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'category',
     ordering = '-id',
     list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 20
     list_max_show_all = 60
     list_editable = 'first_name', 'last_name'
-    list_display_links = 'id', 'phone',
+    list_display_links = 'id',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
